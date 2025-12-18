8a5b833222805c2f91c508a86aa444882c6db430
Nguyễn Quốc Huy <darkness1652004@gmail.com>
2025-12-18 20:21:58 +0700

    [Fix] Lỗi vỡ thanh header khi nhấn vào biểu tượng người dùng

--- includes/header.php
+++ includes/header.php
@@ -17,42 +17,32 @@ $base_url = '/QuanLiSinhVien/';
 </head>
 <body>
 
-<header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="<?php echo $base_url; ?>index.php">
+<header class="navbar navbar-dark sticky-top bg-primary p-0 shadow-sm py-2">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="<?php echo $base_url; ?>index.php">
         <i class="bi bi-mortarboard-fill"></i> StudentMS
     </a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+
+    <ul class="navbar-nav flex-row ms-auto me-3">
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="<?php echo $base_url . 'assets/images/avatars/' . (isset($current_user['avatar']) ? htmlspecialchars($current_user['avatar']) : 'default.png'); ?>" 
+                         alt="Avatar" class="rounded-circle me-2" style="width: 32px; height: 32px; object-fit: cover;">
+                    <span class="d-none d-sm-inline"><?php echo isset($_SESSION['full_name']) ? htmlspecialchars($_SESSION['full_name']) : 'Tài khoản'; ?></span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end shadow-lg border-0" aria-labelledby="navbarDropdown" style="position: absolute;">
+                    <li><a class="dropdown-item py-2" href="<?php echo $base_url; ?>profile.php"><i class="bi bi-person-circle me-2"></i>Thông tin cá nhân</a></li>
+                    <li><a class="dropdown-item py-2" href="<?php echo $base_url; ?>change_password.php"><i class="bi bi-key-fill me-2"></i>Đổi mật khẩu</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item py-2 text-danger" href="<?php echo $base_url; ?>logout.php"><i class="bi bi-box-arrow-right me-2"></i>Đăng xuất</a></li>
+                </ul>
+            </li>
+        <?php else: ?>
+            <li class="nav-item"><a class="nav-link px-3 text-white" href="<?php echo $base_url; ?>login.php">Đăng nhập</a></li>
+        <?php endif; ?>
+    </ul>
+
+    <button class="navbar-toggler d-md-none collapsed border-0" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false">
         <span class="navbar-toggler-icon"></span>
     </button>
-    
-    <div class="w-100"></div>
-
-    <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-            <?php if (isset($_SESSION['user_id'])): ?>
-            <ul class="navbar-nav flex-row">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle pe-3" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="<?php echo $base_url . 'assets/images/avatars/' . (isset($current_user['avatar']) ? htmlspecialchars($current_user['avatar']) : 'default.png'); ?>" alt="Avatar" class="avatar-sm rounded-circle me-1">
-                        <?php echo isset($_SESSION['full_name']) ? htmlspecialchars($_SESSION['full_name']) : 'Tài khoản'; ?>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="<?php echo $base_url; ?>profile.php">
-                            <i class="bi bi-person-circle"></i> Thông tin cá nhân
-                        </a></li>
-                        <li><a class="dropdown-item" href="<?php echo $base_url; ?>change_password.php">
-                            <i class="bi bi-key-fill"></i> Đổi mật khẩu
-                        </a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="<?php echo $base_url; ?>logout.php">
-                            <i class="bi bi-box-arrow-right"></i> Đăng xuất
-                        </a></li>
-                    </ul>
-                </li>
-            </ul>
-            <?php else: ?>
-                <a class="nav-link px-3" href="<?php echo $base_url; ?>login.php">Đăng nhập</a>
-            <?php endif; ?>
-        </div>
-    </div>
 </header>
